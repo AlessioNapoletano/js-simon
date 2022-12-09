bff29d691f25284a6b23ebd643d23d766ea47e33
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-12-09 01:33:34 +0100

    Es 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const mainElement = document.querySelector("main");
 mainElement.classList.add("text-center", "py-5")
 
+const Es = document.createElement("h1");
+Es.classList.add("fw-bold", "text-danger", "mb-5");
+Es.innerHTML = "Esercizio 1 - Conta Click";
+mainElement.append(Es);
+
 const button = document.createElement("button");
+button.classList.add("mb-5")
 mainElement.append(button);
 
 
@@ -21,9 +27,73 @@ button.addEventListener("click", function () {
     button.innerHTML = "#" + count;
 })
 
+const newEs = document.createElement("h1");
+newEs.classList.add("fw-bold", "text-danger");
+newEs.innerHTML = "Esercizio 2 - Cronometro";
+mainElement.append(newEs);
+
 function limit() {
     console.log("azzera");
     count = 0;
     button.innerHTML = "#" + count;
     button.classList.remove("bg-danger")
+}
+
+
+                                                        //ESERCIZIO 2
+let number = 0;
+let clock;
+
+// OUTPUT TIME
+const outputTime = document.createElement("h1");
+outputTime.innerHTML = number;
+mainElement.append(outputTime);
+
+
+//BOTTONE START
+const buttonStart = document.createElement("a");
+buttonStart.classList.add("btn" , "bg-primary" , "m-5");
+buttonStart.innerHTML = "Start";
+mainElement.append(buttonStart);
+
+buttonStart.addEventListener("click" , function() {
+    clock = setInterval(myFunction, 1);
+    outputTime.innerHTML = number;
+    buttonStart.classList.add("d-none");
+
+});
+
+
+// BOTTONE STOP
+const buttonStop = document.createElement("a");
+buttonStop.classList.add("btn" , "bg-danger" , "m-5");
+buttonStop.innerHTML = "Stoppa il tempo";
+mainElement.append(buttonStop);
+
+buttonStop.addEventListener("click", function() {
+    console.log("stop al tempo")
+    clearInterval(clock)
+    outputTime.innerHTML = number;
+    buttonStart.classList.toggle("d-none");
+    
+})
+
+// BOTTONE RESET
+const buttonReset = document.createElement("a");
+buttonReset.classList.add("btn" , "btn-success" , "m-5");
+buttonReset.innerHTML = "Reset";
+mainElement.append(buttonReset);
+
+buttonReset.addEventListener("click" , function() {
+    number = 0;
+    outputTime.innerHTML = number;
+})
+
+
+
+
+
+function myFunction(){
+    number++;
+    outputTime.innerHTML = number;
 }
